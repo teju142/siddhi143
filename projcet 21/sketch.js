bbf0a07074f9050c8e8a5253047459c72e010541
teju142 <87301071+teju142@users.noreply.github.com>
2021-07-29 06:26:51 -0700

    Add files via upload

---
+++ projcet 21/sketch.js
@@ -0,0 +1,69 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var ball,groundObj,rightSide,leftSide;
+var world;
+var radius = 40;
+
+
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1600, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	var ball_options={
+		isStatic:false,
+		restitution:0.3,
+		friction:0,
+		density:1.2
+	}
+
+	ball = Bodies.circle(260,100,radius/2,ball_options);
+	World.add(world,ball);
+
+	groundObj=new ground(width/2,670,width,20);
+	leftSide = new ground(1100,600,20,120);
+	rightSide = new ground(1350,600,20,120);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+
+  ellipse(ball.position.x,ball.position.y,radius,radius);
+
+  groundObj.display();
+  leftSide.display();  
+  rightSide.display();
+  
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+	if (keyCode = m) {
+
+	  Matter.Body.applyForce(ball,ball.position,{x:85,y:-85});
+  
+	}
+}
+
+
+
